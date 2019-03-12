08473186f5060897aa66b0ac35b521c22ad7e509
Clayton Lautier <clayton.lautier@outlook.com>
2019-03-12 15:13:19 +0100

    nuget: fix suffix

--- tools/pack.sh
+++ tools/pack.sh
@@ -2,13 +2,16 @@
 
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 PACKAGE_VERSION_SUFFIX=$(node -p "require('./package.json').versionSuffix")
+VERSION=$PACKAGE_VERSION
 
 if [ -z "$PACKAGE_VERSION_SUFFIX" ] && [ -z "$CI" ]; then
 	PACKAGE_VERSION_SUFFIX=dev
 fi
 
-VERSION=$PACKAGE_VERSION-$PACKAGE_VERSION_SUFFIX
+if [ -n "$PACKAGE_VERSION_SUFFIX" ]; then
+VERSION=$VERSION-$PACKAGE_VERSION_SUFFIX
+fi
 
 echo -e "\e[36m ---- Packing '$VERSION' ---- \e[39m"
 
-dotnet pack -p:PackageVersion=$VERSION -p:AssemblyVersion=$PACKAGE_VERSION -o ../../ -c release --include-symbols
+#dotnet pack -p:PackageVersion=$VERSION -p:AssemblyVersion=$PACKAGE_VERSION -o ../../ -c release --include-symbols
