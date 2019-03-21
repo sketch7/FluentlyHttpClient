5e9ff011dc59c569925bb8b3a5b7eb8a934cae28
Clayton Lautier <clayton.lautier@outlook.com>
2019-03-21 14:08:29 +0100

    build: improved build pack and release process

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0` # Full path of this script
+DIR=`dirname "${THIS}"` # This directory path
+# imports
+. "$DIR/version-builder.sh"
+
 err(){
 	echo -e "\e[31m $* \e[0m" >>/dev/stderr
 }
@@ -8,4 +14,11 @@ if [ "$SKETCH7_NUGET_API_KEY" == "" ]; then
 	err "'SKETCH7_NUGET_API_KEY' environment variable not defined."
 	exit 1
 fi
+
+echo -e "\e[36m ---- Publishing '$VERSION' ---- \e[39m"
+
 find *.nupkg | xargs -i dotnet nuget push {} -k $SKETCH7_NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+
+echo -e "\e[36m ---- git tag '$VERSION' ---- \e[39m"
+git tag $VERSION
+git push --tags
