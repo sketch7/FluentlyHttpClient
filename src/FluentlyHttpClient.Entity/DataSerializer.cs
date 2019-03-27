74661b26d0e413b61fb040a7a4aaadf41e3becd2
Clayton Lautier <clayton.lautier@outlook.com>
2019-03-27 14:06:40 +0100

    entity: update libs

--- src/FluentlyHttpClient.Entity/DataSerializer.cs
+++ src/FluentlyHttpClient.Entity/DataSerializer.cs
@@ -7,25 +7,10 @@ namespace FluentlyHttpClient.Entity
 	{
 		private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
 		{
-			ContractResolver = new DefaultContractResolver(),
-			//TypeNameHandling = TypeNameHandling.Objects
+			ContractResolver = new DefaultContractResolver()
 		};
 
 		public static string Serialize<TItem>(TItem value) => JsonConvert.SerializeObject(value, Settings);
 		public static TResult Deserialize<TResult>(string value) => JsonConvert.DeserializeObject<TResult>(value, Settings);
-
-		//public static string Serialize<TItem>(TItem value)
-		//{
-		//	var data = MessagePack.MessagePackSerializer.Serialize(value, MessagePack.Resolvers.ContractlessStandardResolver.Instance);
-		//	var result = System.Text.Encoding.UTF8.GetString(data);
-		//	return result;
-		//}
-
-		//public static TResult Deserialize<TResult>(string value)
-		//{
-		//	var data = System.Text.Encoding.UTF8.GetBytes(value);
-		//	var result = MessagePack.MessagePackSerializer.Deserialize<TResult>(data, MessagePack.Resolvers.ContractlessStandardResolver.Instance);
-		//	return result;
-		//}
 	}
 }
