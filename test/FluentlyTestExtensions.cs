b4b2d1ed7257b0ebc4a33dd08a1c1f5250afc05d
Stephen Lautier <stephen.lautier@outlook.com>
2026-04-20 12:05:48 +0200

    feat: add Microsoft.Data.SqlClient package and update references for improved database support

--- test/FluentlyTestExtensions.cs
+++ test/FluentlyTestExtensions.cs
@@ -5,6 +5,7 @@ public static class FluentlyTestExtensions
 	public static FluentHttpClientBuilder WithMockMessageHandler(this FluentHttpClientBuilder builder, MockHttpMessageHandler? handler = null)
 	{
 		handler ??= new();
+		handler.Fallback.Respond(HttpStatusCode.OK);
 		return builder.WithMessageHandler(handler);
 	}
 }
